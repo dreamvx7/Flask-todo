4a9e7f2c7b587f35003785dde2d5194474256e98
dreamvx7 <dreamvx7@gmail.com>
2016-06-01 21:51:47 +0800

    Add files via upload

--- app/views.py
+++ app/views.py
@@ -4,4 +4,4 @@ from flask import render_template
 
 @app.route('/')
 def index():
-    return render_template("index.html", text="hello world")
+    return render_template("index.html",text="hello world")

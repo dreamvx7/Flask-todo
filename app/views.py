78841245723634ec00a37e6ddced8681a5779c7c
dreamvx7 <dreamvx7@gmail.com>
2016-06-02 21:59:57 +0800

    Add files via upload

--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
 from app import app
-
 from flask import render_template
+from models import Todo
 
 @app.route('/')
 def index():
+    todos = Todo.objects.all()
     return render_template("index.html",text="hello world")
